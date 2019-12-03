0de9055c4de15102bbb35515949c4396bf7b0f8f
Philipp von Hammerstein <p.vonhammerstein@whu.edu>
2019-12-03 22:09:01 +0100

    Flow: Create function

---
+++ 03_functions/own_function.py
@@ -0,0 +1,12 @@
+def increase(number):
+  print('Increasing the number..')
+  print('..almost there..')
+  print('..got it!')
+  return number + 1
+  pass
+
+
+print(increase(29))
+
+# Functions enable devs to get rid of duplicate code
+# Use returns! Default otherwise is None
